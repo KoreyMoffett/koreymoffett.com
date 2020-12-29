56d00c847fa088d284359cb6d0ceede573e13ae6
Korey Moffett <korey.moffett87@gmail.com>
2020-12-29 00:43:17 -0600

    Fix typo

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
             <h1 class="title">About</h1><br>
             <div class="notification">
                 <h1 class="subtitle">My name is Korey, I enjoy coding and tinkering with technology.<br><br>
-                    <h1 class="subtitle">If you would like to contact me <a href="mailto: korey.moffett87@gmail.com">Email</a>, <a href="https://fosstodon.org/@koreymoffett">Mastodon</a> or <a href="https://twitter.com/korey_moffett">Twitter</a> the the best methods to do so.</h1>
+                    <h1 class="subtitle">If you would like to contact me <a href="mailto: korey.moffett87@gmail.com">Email</a>, <a href="https://fosstodon.org/@koreymoffett">Mastodon</a> or <a href="https://twitter.com/korey_moffett">Twitter</a> are the best ways to do so.</h1>
                 </h1>
             </div>
         </div>
