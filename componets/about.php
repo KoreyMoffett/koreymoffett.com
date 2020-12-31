193bfdc07b7eb1b3f210f8c777e04938d4cfeb0c
Korey Moffett <korey.moffett87@gmail.com>
2020-12-31 12:27:22 -0600

    Typo

---
+++ componets/about.php
@@ -0,0 +1,18 @@
+
+<?php
+echo '
+
+ <!-- About section -->
+
+    <section class="section">
+        <div class="container">
+            <h1 class="title">About</h1><br>
+            <div class="notification">
+                <h1 class="subtitle">My name is Korey, I enjoy coding and tinkering with technology.<br><br>
+                    <h1 class="subtitle">If you would like to contact me <a href="mailto: korey.moffett87@gmail.com">Email</a>, <a href="https://fosstodon.org/@koreymoffett">Mastodon</a> or <a href="https://twitter.com/korey_moffett">Twitter</a> are the best ways to do so.</h1>
+                </h1>
+            </div>
+        </div>
+    </section>
+'
+?>
